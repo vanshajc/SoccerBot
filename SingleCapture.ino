691038cb0f1bf6833cbe047131bd6cc13d0b770d
Vanshaj Chowdhary <vchowdhary@wpi.edu>
2017-07-23 18:07:50 -0400

    Add base code for single capture from ArduCam Mini

---
+++ SingleCapture.ino
@@ -0,0 +1,116 @@
+#include <Wire.h>
+#include <ArduCAM.h>
+#include <SPI.h>
+#include "memorysaver.h"
+#define BMPIMAGEOFFSET 66
+const char bmp_header[BMPIMAGEOFFSET] PROGMEM =
+{
+  0x42, 0x4D, 0x36, 0x58, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x42, 0x00, 0x00, 0x00, 0x28, 0x00,
+  0x00, 0x00, 0x40, 0x01, 0x00, 0x00, 0xF0, 0x00, 0x00, 0x00, 0x01, 0x00, 0x10, 0x00, 0x03, 0x00,
+  0x00, 0x00, 0x00, 0x58, 0x02, 0x00, 0xC4, 0x0E, 0x00, 0x00, 0xC4, 0x0E, 0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF8, 0x00, 0x00, 0xE0, 0x07, 0x00, 0x00, 0x1F, 0x00,
+  0x00, 0x00
+};
+// set pin 7 as the slave select for the digital pot:
+const int CS = 7;
+bool is_header = false;
+int mode = 0;
+uint8_t start_capture = 1;
+#if defined (OV2640_MINI_2MP)
+  ArduCAM myCAM( OV2640, CS );
+#elif defined (OV3640_MINI_3MP)
+ArduCAM myCAM( OV3640, CS );
+#else
+  ArduCAM myCAM( OV5642, CS );
+#endif
+uint8_t read_fifo_burst(ArduCAM myCAM);
+uint8_t temp;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Wire.begin();
+  pinMode(CS, OUTPUT);
+  SPI.begin();
+  while(1){
+  //Check if the ArduCAM SPI bus is OK
+  myCAM.write_reg(ARDUCHIP_TEST1, 0x55);
+  temp = myCAM.read_reg(ARDUCHIP_TEST1);
+  if (temp != 0x55){
+    //Serial.println(F("ACK CMD SPI interface Error!"));
+    delay(1000);continue;
+  }else{
+    //Serial.println(F("ACK CMD SPI interface OK."));
+    break;
+  }
+  }
+  
+   myCAM.OV2640_set_JPEG_size(OV2640_320x240);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+ if (start_capture == 1)
+  {
+    myCAM.flush_fifo();
+    myCAM.clear_fifo_flag();
+    //Start capture
+    myCAM.start_capture();
+    start_capture = 0;
+  }
+  if (myCAM.get_bit(ARDUCHIP_TRIG, CAP_DONE_MASK))
+  {
+   // Serial.println(F("ACK CMD CAM Capture Done."));
+    read_fifo_burst(myCAM);
+    //Clear the capture done flag
+    myCAM.clear_fifo_flag();
+    //start_capture = 1;
+  }
+}
+
+
+uint8_t read_fifo_burst(ArduCAM myCAM)
+{
+  uint8_t temp = 0, temp_last = 0;
+  uint32_t length = 0;
+  length = myCAM.read_fifo_length();
+  //Serial.println(length, DEC);
+  if (length >= MAX_FIFO_SIZE) //512 kb
+  {
+    //Serial.println(F("Over size."));
+    return 0;
+  }
+  if (length == 0 ) //0 kb
+  {
+    //Serial.println(F("Size is 0."));
+    return 0;
+  }
+  myCAM.CS_LOW();
+  myCAM.set_fifo_burst();//Set fifo burst mode
+  temp =  SPI.transfer(0x00);
+  length --;
+  while ( length-- )
+  {
+    temp_last = temp;
+    temp =  SPI.transfer(0x00);
+    if (is_header == true)
+    {
+      Serial.write(temp);
+    }
+    else if ((temp == 0xD8) & (temp_last == 0xFF))
+    {
+      is_header = true;
+      //Serial.println(F("ACK IMG"));
+      Serial.write(temp_last);
+      Serial.write(temp);
+    }
+    if ( (temp == 0xD9) && (temp_last == 0xFF) ) //If find the end ,break while,
+    break;
+    delayMicroseconds(15);
+  }
+  myCAM.CS_HIGH();
+  is_header = false;
+  return 1;
+}
